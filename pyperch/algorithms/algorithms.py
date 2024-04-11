2bd4067254dc8e5c97f074dc8ba38435c6843633
John <jmansfield6@gatech.edu>
2024-04-11 12:04:09 -0400

    adding ability to change num of hidden layers

--- pyperch/algorithms/algorithms.py
+++
@@ -1,9 +0,0 @@
-# Author: Genevieve Hayes
-# License: BSD 3 clause
-
-# Modified by John Mansfield
-
-#copy from https://github.com/gkhayes/mlrose/blob/master/mlrose/algorithms.py
-
-#assume continuous opt problem, pull functions
-#from here https://github.com/gkhayes/mlrose/blob/master/mlrose/opt_probs.py
--- pyperch/neural/rhc_nn.py
+++ pyperch/neural/rhc_nn.py
@@ -10,27 +10,33 @@ import copy
 
 
 class RHCModule(nn.Module):
-    def __init__(self, input_dim=20, output_dim=2, hidden_units=10, nonlin=nn.ReLU()):
+    def __init__(self, input_dim=20, output_dim=2, hidden_units=10, hidden_layers=1, dropout_percent=0, lr=.1, activation=nn.ReLU(), output_activation=nn.Softmax(dim=-1)):
         super().__init__()
 
         self.input_dim = input_dim
         self.output_dim = output_dim
         self.hidden_units = hidden_units
+        self.hidden_layers = hidden_layers
+        self.layers = nn.ModuleList()
+        self.activation = activation
+        self.dropout = nn.Dropout(dropout_percent)
+        self.softmax = output_activation
+        self.lr = lr
 
-        #any input params can automagically be grid searched
-        #TODO: add RHC specific params/add ability to dynamically add hidden layers
-        self.dense0 = nn.Linear(self.input_dim, self.hidden_units)
-        self.nonlin = nonlin
-        self.dropout = nn.Dropout(0.5)
-        self.dense1 = nn.Linear(self.hidden_units, self.hidden_units)
-        self.output = nn.Linear(self.hidden_units, self.output_dim)
-        self.softmax = nn.Softmax(dim=-1)
+        #TODO: add RHC specific params/any input params can automagically be grid searched
+
+        self.layers.append(nn.Linear(self.input_dim, self.hidden_units))
+        for layer in range(self.hidden_layers):
+            self.layers.append(nn.Linear(self.hidden_units, self.hidden_units))
+        self.layers.append(nn.Linear(self.hidden_units, self.output_dim))
 
     def forward(self, X, **kwargs):
-        X = self.nonlin(self.dense0(X))
+        X = self.activation(self.layers[0](X))
         X = self.dropout(X)
-        X = self.nonlin(self.dense1(X))
-        X = self.softmax(self.output(X))
+        for i in range(self.hidden_layers):
+            X = self.activation(self.layers[i+1](X))
+        #X = self.activation(self.layers[1](X))
+        X = self.softmax(self.layers[self.hidden_layers+1](X))
         return X
 
     def run_rhc_single_step(self, net, X_train, y_train, **fit_params):
@@ -40,15 +46,16 @@ class RHCModule(nn.Module):
         y_pred = net.infer(X_train, **fit_params)
         old_loss = net.get_loss(y_pred, y_train, X_train, training=False)
 
-        lr=.1
+        lr=self.lr
         #todo: randomly select tensor instead of hidden only
         #todo: optimize
-        input_dim = np.random.randint(0, self.hidden_units)
-        output_dim = np.random.randint(0, self.hidden_units)
+        layer = np.random.randint(0, len(self.layers))-1
+        input_dim = np.random.randint(0, net.module_.layers[layer].weight.shape[0])
+        output_dim = np.random.randint(0, net.module_.layers[layer].weight.shape[1])
         neighbor = lr * np.random.choice([-1, 1])
 
         with torch.no_grad():
-            net.module_.dense1.weight[input_dim][output_dim] = neighbor + net.module_.dense1.weight[input_dim][output_dim].data
+            net.module_.layers[layer].weight[input_dim][output_dim] = neighbor + net.module_.layers[layer].weight[input_dim][output_dim].data
 
         # Evaluate the new loss
         y_pred = net.infer(X_train, **fit_params)
