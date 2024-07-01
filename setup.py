7e14abe18be50e1a8eb8d878c59758da6e8ce4ac
John <jmansfield6@gatech.edu>
2024-07-01 14:33:20 -0400

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyperch',
+    url='https://github.com/jlm429/pyperch',
+    version='0.1.0',
+    platforms=['Any'],
+    license='New BSD',
+    author='John Mansfield',
+    author_email='jlm429@gmail.com',
+    description='Randomized opt networks with PyTorch',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    include_package_data=True,
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    install_requires=[
+        'skorch==0.15.0',
+        'torch>=2.2, <2.4',
+        'numpy',
+        'matplotlib'
+    ],
+)
