4f286c7637bf31ded06d3e54fdf0e2c73312ee9c
John <jmansfield6@gatech.edu>
2025-12-11 04:06:34 -0500

    chore: sync packaging metadata and clean up dependencies (Poetry + setup.py)

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="pyperch",
+    version="0.2.1",
+    url="https://github.com/jlm429/pyperch",
+    author="John Mansfield",
+    author_email="jlm429@gmail.com",
+    description="Randomized optimization networks with PyTorch",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    platforms=["Any"],
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires=">=3.10",
+    install_requires=[
+        "torch",
+        "numpy",
+        "matplotlib",
+        "scikit-learn>=1.7.2,<2.0.0",
+        "optuna>=3.6.0,<4.0.0",
+    ],
+)
